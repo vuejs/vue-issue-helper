4b8c1ebf3ee959436cce039e2ca3157313932a33
Evan You <yyx990803@gmail.com>
2017-03-19 15:54:05 +0800

    small i18n improvements

--- src/i18n/plugin.js
+++ src/i18n/plugin.js
@@ -2,7 +2,8 @@ import marked from 'marked'
 import locales from './locales/index'
 
 export default Vue => {
-  // add locale and setLang() method to root instance
+  // add locale and lang to root instance.
+  // $lang is reactive.
   Vue.mixin({
     beforeCreate () {
       if (this.$root === this) {
@@ -12,23 +13,28 @@ export default Vue => {
     }
   })
 
+  const notFound = (lang, id) =>
+    `[i18n content not found for { lang: "${lang}", id: "${id}" }`
+
   // global i18n method for simple phrases in text interpolations
   Vue.prototype.i18n = function (id) {
-    const locale = this.$root.$locales[this.$root.$lang]
-    return locale[id] || '!!! i18n content not found !!!'
+    const { $locales, $lang } = this.$root
+    const locale = $locales[$lang]
+    return locale[id] || notFound($lang, id)
   }
 
   // component for rendering an i18n locale markdown file
   Vue.component('i18n', {
     props: ['id'],
     render (h) {
-      const locale = this.$root.$locales[this.$root.$lang]
+      const { $locales, $lang } = this.$root
+      const locale = $locales[$lang]
       const content = locale[this.id]
       return h('div', {
         domProps: {
           innerHTML: content
             ? marked(content.trim())
-            : '<div style="color:red">i18n content not found</div>'
+            : `<div style="color:red">${notFound($lang, this.id)}</div>`
         }
       })
     },
