e038a0c63bc415d9600471c5d2cd4ec3d9e67753
Evan You <yyx990803@gmail.com>
2018-01-13 13:22:40 -0500

    add vue-cli

--- src/config/repos.js
+++ src/config/repos.js
@@ -3,7 +3,8 @@ const repos = [
   { id: 'vuejs/vuex', name: 'vuex' },
   { id: 'vuejs/vue-router', name: 'vue-router' },
   { id: 'vuejs/vue-loader', name: 'vue-loader' },
-  { id: 'vuejs/vue-test-utils', name: 'vue-test-utils' }
+  { id: 'vuejs/vue-test-utils', name: 'vue-test-utils' },
+  { id: 'vuejs/vue-cli', name: 'vue-cli' }
 ]
 
 export { repos }
